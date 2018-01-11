6dcdb9bc12b8e4081aeba9c25dca4de59d020068
Euna <xlpczv@gmail.com>
2018-01-11 19:24:16 +0900

    chapter2
    perceptron의 weights를 #에서처럼 설정하여 경계 값을 이용하면 AND, NAND, OR 값은 각각 잘 나오지만 XOR에서 값이 제대로 나오지 않는 문제 발생

---
+++ Chapter2.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5]) #1,1
+    b = -0.7 #-1
+    perceptron = np.sum(w*x) + b
+    if perceptron > 0:
+        return 1
+    else:
+        return 0
+
+print("AND(1,1):", AND(1,1), "AND(1,0):", AND(1,0), "AND(0,1):", AND(0,1), "AND(0,0):", AND(0,0))
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5]) #-1,-1
+    b = 0.7 #1
+    perceptron = np.sum(w*x) + b
+    if perceptron > 0:
+        return 1
+    else:
+        return 0
+
+print("NAND(1,1):", NAND(1,1), "NAND(1,0):", NAND(1,0), "NAND(0,1):", NAND(0,1), "NAND(0,0):", NAND(0,0))
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5]) #1,1
+    b = -0.2 #0
+    perceptron = np.sum(w*x) + b
+    if perceptron > 0:
+        return 1
+    else:
+        return 0
+
+print("OR(1,1):", OR(1,1), "OR(1,0):", OR(1,0), "OR(0,1):", OR(0,1), "OR(0,0):", OR(0,0))
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print("XOR(1,1):", XOR(1,1), "XOR(1,0):", XOR(1,0), "XOR(0,1):", XOR(0,1), "XOR(0,0):", XOR(0,0))
